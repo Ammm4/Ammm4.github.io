f0f08fe6f4bc377f4049620a0964eb038534b90b
Ammm4 <ammm.4amit@gmail.com>
2021-03-04 11:28:53 +0000

    socket component add

---
+++ src/components/socket/Socket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Socket.css';
+
+function Socket(){
+   return(
+       <footer className="socket">                
+                  <p>Created by Amiit Rana | &#169; 2017 All Rights Reserved</p>                  
+      </footer>
+
+   )
+}
+export default Socket;
