2c45e28f178d02f9ce49aff50280f12c1555c7f2
Ammm4 <ammm.4amit@gmail.com>
2021-03-04 11:37:45 +0000

    project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
 
+//Import Components
+import Nav from './components/navbar/Nav';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Skills from './components/skills/Skills';
+import Socket from './components/socket/Socket';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Nav />
+      <Home />
+     <About />
+     <Portfolio />
+     <Skills />
+     <Contact />
+     <Socket />
     </div>
   );
 }
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,6 +6,11 @@ function About() {
   return (
     <div class="about">
          <div class="about-heading"> <span>ABOUT ME</span></div>
+         <div className="about-content">
+         <p>I am a highly competent IT professional with a proven track record in designing websites, networking and managing databases. I have strong technical skills as well as excellent interpersonal skills, enabling me to interact with a wide range of clients. I am eager to be challenged in order to grow and further improve my IT skills. My greatest passion is in life is using my technical know-how to benefit other people and organisations.</p>
+         <p>I did my learning from Codeacademy, W3Schools and freeCodeCamp. I am certified from these institutes. I am currently enrolled in many other backend courses and learning various technology. My aim is to become full stack Developer. I am working on various projects at the moment which you can find in Github.</p>
+         <div><button>GET CV</button></div>    
+         </div>
     </div>
   )
 
