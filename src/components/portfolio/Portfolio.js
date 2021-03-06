b9a03bdde43faa7bbf59a30517e4aac1c0497d44
Ammm4 <ammm.4amit@gmail.com>
2021-03-06 12:25:34 +0000

    portfolio section js finish

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import './Portfolio.css'
+import './Portfolio.css';
+import img1 from '../images/home1.jpg';
+import img2 from '../images/home.jpg';
+import img3 from '../images/home2.png';
+import img4 from '../images/home3.jpg';
 
 
 function Portfolio(){
   return (
     <div className="portfolio">
         <div className="portfolio-heading"><span>MY PROJECTS</span></div>
+        <div className="portfolio-content">
+             <div className="latest-project">
+                  <div className="latest"></div> 
+             </div>
+             <div className="project-1">
+                <div className="project"></div> 
+             </div>
+              <div className="project-2">
+                <div className="project"></div> 
+              </div>
+              <div className="project-3">
+               <div className="project"></div> 
+             </div>
+             <div className="project-4">
+                <div className="project"></div>
+             </div>
+             
+        </div>
     </div>
   )
 }
