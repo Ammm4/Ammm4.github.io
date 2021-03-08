74b2ba643b6d4d8e5fadd017ba091eadfc7d182d
Ammm4 <ammm.4amit@gmail.com>
2021-03-08 14:04:30 +0000

    app component change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import './App.css';
 
 //Import Components
@@ -9,7 +10,34 @@ import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Skills from './components/skills/Skills';
 import Socket from './components/socket/Socket';
-function App() {
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.showNav = this.showNav.bind(this);
+  }
+  showNav() {
+    document.querySelector('.navbar .fa-bars').classList.toggle('fa-times');
+    document.querySelector('.navbar .nav').classList.toggle('nav-toggle');
+  }
+  render() {
+    return (
+        <div className="App">
+            <Nav showNav={this.showNav} />
+            <Home />
+            <About />
+            <Portfolio />
+            <Skills />
+            <Contact />
+            <Socket />
+    </div>
+    )
+  }
+}
+
+
+
+/* function App() {
   return (
     <div className="App">
       <Nav />
@@ -21,6 +49,6 @@ function App() {
      <Socket />
     </div>
   );
-}
+} */
 
 export default App;
