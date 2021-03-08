4869a7ba36beef248331c6f2eb88561a59ffd8a2
Ammm4 <ammm.4amit@gmail.com>
2021-03-08 14:00:51 +0000

    Nav section complete

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import './Nav.css';
 
-function Nav (){
+class Nav extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+      this.props.showNav();
+    }
+  render() {
+    return (
+      <div className="navbar">
+          <div className="brand">1A.R</div>
+          <div className="fas fa-bars" onClick={this.handleClick}></div>
+          <div className="nav">
+            <ul className="navlist">
+              <li><a href="#portfolio">PORTFOLIO</a></li>
+              <li><a href="#skills">SKILLS</a></li>
+              <li><a href="#contact">CONTACT</a></li>
+            </ul>
+          </div>
+
+    </div>
+  )
+
+    
+  }
+}
+
+
+
+
+/* function Nav (){
   return(
     <div className="navbar">
       <div className="brand">1A.R</div>
+      <div className="fas fa-bars"></div>
       <div className="nav">
         <ul className="navlist">
-          <li><a href="#">PORTFOLIO</a></li>
-          <li><a href="#">SKILLS</a></li>
-          <li><a href="#">CONTACT</a></li>
+          <li><a href="#portfolio">PORTFOLIO</a></li>
+          <li><a href="#skills">SKILLS</a></li>
+          <li><a href="#contact">CONTACT</a></li>
         </ul>
       </div>
 
     </div>
   )
-}
+} */
 
 export default Nav;
