1d4299ba667395250362a78f70aea99e048b6da2
Ammm4 <ammm.4amit@gmail.com>
2021-03-12 21:59:36 +0000

    contact section refactor

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,40 +1,35 @@
 import React from 'react';
+import {contactLists,socialProfiles} from './contactList'
 import './Contact.css';
 
 function Contact(){
   return(
     <div id="contact" className="contact">
       <div className="contact-heading"> <span>CONTACT ME</span></div>
-      <div className="contact-content">                       
-               <div className="address">
-               <i className="fas fa-map-marker-alt"> </i>
-               <span> <b>My Address</b></span>
-                     <p>
-                        Cwmbran, Oakfield <br/>
-                        Torfaen, Gwent <br/>
-                        Wales, UK
-                     </p>
-               </div>
-               
-               <div className="telephone">
-                   <div className="icon-container"><i className="fas fa-phone-alt"></i><span><b> Call me</b></span></div>       
-                   <p className="para">+44 07780335313</p>                 
-               </div>
-                
-               <div className="email">  
-                  <i className="fas fa-envelope"> </i><span> <b>Mail me</b></span>  
-                  <p>ammm.4amit@gmail.com</p>
-               </div>    
-               <div className="social-media">
+      <div className="contact-content">
+                {contactLists.map((item,index) => {
+                   return(
+                        <div className="contact-detail" key={index}>
+                            <i className={item.iconName}></i>
+                            <span> <b>{item.title}</b></span>
+                            {item.p.map((para) => {
+                              return(<p>{para}</p>)
+                            })}
+                        </div>
+                   )
+                })}
+                 <div className="social-media">
                   <p>
-                    <a href="#"  target="_blank"><i className="fab fa-facebook"></i></a>
-                    <a href="#"  target="_blank"><i className="fab fa-github"></i></a>
-                    <a href="#"  target="_blank"><i className="fab fa-linkedin"></i></a>
+                    {socialProfiles.map((item,index) => {
+                         return(
+                          <a href={item.url} target="_blank" key={index}>
+                            <i className={item.cName}></i>
+                          </a>
+                         ) 
+                    })}
                  </p>
-               </div>
-      </div>
-      
-           
+               </div>                                         
+      </div>         
     </div>
   )
 }
---
+++ src/components/contact/contactList.js
@@ -0,0 +1,32 @@
+export const contactLists = [
+         {
+           title: 'My Address',
+           iconName: 'fas fa-map-marker-alt',
+           p: ['Cwmbran, Oakfield', 'Torfaen, Gwent', 'Wales, UK']
+         },
+         {
+          title: 'Call Me',
+          iconName: 'fas fa-phone-alt',
+          p: ['+44 07780335313']
+         },
+         {
+          title: 'Mail Me',
+          iconName: 'fas fa-envelope',
+          p: ['ammm.4amit@gmail.com']
+         }
+];
+
+export const socialProfiles = [
+               {
+                 cName: 'fab fa-facebook',
+                 url: '#'
+               },
+               {
+                cName: 'fab fa-github',
+                url: '#'
+               },
+               {
+                cName: 'fab fa-linkedin',
+                url: '#'
+              }
+]
